820ba7b629ed909c9da584830003417fe6284d13
Alejoram2505 <Ram23601@uvg.edu.gt>
2024-03-18 21:08:08 -0600

    agregar la clase association

---
+++ src/main/java/uvg/ed/gt/Association.java
@@ -0,0 +1,20 @@
+package uvg.ed.gt;
+
+public class Association<K, V> {
+    private K key;
+    private V value;
+
+    public Association(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
+
