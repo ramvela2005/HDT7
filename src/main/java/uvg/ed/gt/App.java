bbaf93353c04c0801c5be7db663af4eb8114f722
Alejoram2505 <Ram23601@uvg.edu.gt>
2024-03-18 20:38:30 -0600

    Initial commit

---
+++ src/main/java/uvg/ed/gt/App.java
@@ -0,0 +1,13 @@
+package uvg.ed.gt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
